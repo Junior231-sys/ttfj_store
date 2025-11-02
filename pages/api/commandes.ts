059b34e3004f65c80a58eed7c3c72a5781760d05
Junior Tiboncho <junior231@gmail.com>
2025-11-02 12:06:10 +0100

    Nettoyage des fichiers ignorés

--- pages/api/commandes.ts
+++
@@ -1,8 +0,0 @@
-// pages/api/commandes.ts
-import { NextApiRequest, NextApiResponse } from 'next'
-
-// Exemple d’API qui renvoie la liste des commandes (vide pour le moment)
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const orders: any[] = [] // Tu peux plus tard remplacer par ta vraie logique
-  res.status(200).json({ orders })
-}
